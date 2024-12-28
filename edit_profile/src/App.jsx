550fb28679aaf1015a14b2f660f3a477ca619a35
manje22 <uvodicmariela@gmail.com>
2024-12-28 14:40:10 +0100

    komponenta za editiranje profila

---
+++ edit_profile/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import ProfileEditor from './components/ProfileEditor'
+
+function App() {
+  return (
+    <>
+      <ProfileEditor></ProfileEditor>
+    </>
+  )
+}
+
+export default App
